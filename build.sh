900941d9c302786f270461ca222e96b34522f5f4
Adrian Taylor <adetaylor@chromium.org>
2023-07-04 16:33:52 +0000

    Reproduce the problem

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@ set -e
 LLVM_BIN=~/chromium/src/third_party/llvm-build/Release+Asserts/bin
 rm -Rf out
 mkdir out
-$LLVM_BIN/clang -c -o out/a.o a.c
+$LLVM_BIN/clang  -flto=thin -O2 -c -o out/a.o a.c
 pushd out
 $LLVM_BIN/llvm-ar r a.a a.o
 popd
-$LLVM_BIN/clang -c -o out/main.o main.c
-$LLVM_BIN/clang -o out/wholething out/main.o out/a.a
+$LLVM_BIN/clang  -flto=thin -O2 -c -o out/main.o main.c
+$LLVM_BIN/clang  -flto=thin -fuse-ld=lld -O2 -Wl,-plugin-opt=thinlto-index-only=out/index.rsp -o out/wholething out/main.o out/a.a
+$LLVM_BIN/clang  -flto=thin -fuse-ld=lld -O2 @out/index.rsp -o out/wholething out/main.o out/a.a
 
